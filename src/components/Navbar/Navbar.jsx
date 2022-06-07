09d9a68eb15181d131ceef23983a0ca2b1c9a1d7
Shahrukh <themrsrk@gmail.com>
2022-06-07 17:31:01 +0500

    Test Commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,11 +10,11 @@ const Navbar = () => (
       <img src={images.gericht} alt="App Logo" />
     </div>
     <ul className="app__navbar-links">
-      <li className="p__opensans"><a href='#home'>Home</a></li>
-      <li className="p__opensans"><a href='#about'>About</a></li>
-      <li className="p__opensans"><a href='#menu'>Menu</a></li>
-      <li className="p__opensans"><a href='#awards'>Awards</a></li>
-      <li className="p__opensans"><a href='#contact'>Contact</a></li>
+      <li className="p__opensans"><a href='#home'>Home </a></li>
+      <li className="p__opensans"><a href='#about'>About </a></li>
+      <li className="p__opensans"><a href='#menu'>Menu </a></li>
+      <li className="p__opensans"><a href='#awards'>Awards </a></li>
+      <li className="p__opensans"><a href='#contact'>Contact </a></li>
     </ul>
   </nav>
 );
