d7882713aff895a071aa37ce59b0829826951805
Shahrukh <themrsrk@gmail.com>
2022-06-11 16:23:36 +0500

    Completed

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,10 +1,44 @@
 import React from 'react';
-
+import {FooterOverlay, Newsletter} from '../../components'
+import {images} from '../../constants'
+import {FiTwitter, FiFacebook, FiInstagram} from 'react-icons/fi'
 import './Footer.css';
 
 const Footer = () => (
-  <div>
-    Footer
+  <div className="app__footer section__padding">
+    <FooterOverlay/>
+    <Newsletter/>
+
+    <div className="app__footer-links">
+      <div className="app__footer-links-contact">
+          <h1 className="app__footer-headtext">Contact Us</h1>
+          <p className='p__opensans'> 1097 Brooklyn, 23th Street Ave. NewYork City</p>
+          <p className='p__opensans'>+1 780-2988-9076</p>
+          <p className='p__opensans'>+1 780-2321-3376</p>
+
+      </div>
+      <div className="app__footer-links-logo">
+            <img src={images.gericht} alt="Gericht" />
+            <p className="p__opensans">The Best Way To Find Yourself Is To Lose Yourself In The Service Of Others.</p>
+            <img src={images.spoon} alt="Spoon"  style={{marginTop:'15'}}/>
+            <div className="app__footer-links-icons">
+              <FiFacebook/>
+              <FiTwitter/>
+              <FiInstagram/>
+            </div>
+      </div>
+      <div className="app__footer-links-work">
+      <h1 className="app__footer-headtext">Working Hours</h1>
+          <p className='p__opensans'> Monday - Friday</p>
+          <p className='p__opensans'>10:00 am to 02:00 am</p>
+          <p className='p__opensans'>Saturday - Sunday</p>
+          <p className='p__opensans'>12:00 pm to 03:00 am</p>
+      </div>
+    </div>
+
+    <div className="app__footer-copyright">
+        <p className="p__opensans">2021 Gericht. All rights reserved.</p>
+    </div>
   </div>
 );
 
--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,48 +1,47 @@
 import React from 'react';
-import { BsInstagram, BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs'
-import { images } from '../../constants'
-import { SubHeading } from '../../components'
+import { BsInstagram, BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs';
+
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
 import './Gallery.css';
 
 const Gallery = () => {
-  const scrollRef = React.useReducer(null);
+  const scrollRef = React.useRef(null);
+
   const scroll = (direction) => {
-    const { current } = scrollRef
+    const { current } = scrollRef;
 
     if (direction === 'left') {
-      current.scrollLeft -= 300
+      current.scrollLeft -= 300;
     } else {
-      current.scrollLeft += 3000
+      current.scrollLeft += 300;
     }
-  }
-  const gallery_images = [images.gallery01, images.gallery02, images.gallery03, images.gallery04]
+  };
+
   return (
-    <div className="flex__center app__gallery">
+    <div className="app__gallery flex__center">
       <div className="app__gallery-content">
         <SubHeading title="Instagram" />
-        <h1 className="headtext__cormorant"> Photo Gallery</h1>
-        <p className='p__opensans' style={{ color: '#AAA', marginTop: '2rem' }}>lorem ipsum dolor sit amet, consectetur adip Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum corporis velit cum commodi sed, sit eius quia voluptas rem dicta?</p>
-        <button type="button" className="custom__button">View more</button>
+        <h1 className="headtext__cormorant">Photo Gallery</h1>
+        <p className="p__opensans" style={{ color: '#AAAAAA', marginTop: '2rem' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mu.</p>
+        <button type="button" className="custom__button">View More</button>
       </div>
-
       <div className="app__gallery-images">
-        <div className="app__gallery-images-container" ref={scrollRef}>
-          {gallery_images.map((image, index) => {
-            return (
-              <div className="app__gallery-images-card flex__center" key={`gallery_image-${index + 1}`}>
-                < img src={image} alt="Gallery Image" />
-                <BsInstagram className="app__gallery-image-icon" />
-              </div>
-            )
-          })}
+        <div className="app__gallery-images_container" ref={scrollRef}>
+          {[images.gallery01, images.gallery02, images.gallery03, images.gallery04].map((image, index) => (
+            <div className="app__gallery-images_card flex__center" key={`gallery_image-${index + 1}`}>
+              <img src={image} alt="gallery_image" />
+              <BsInstagram className="gallery__image-icon" />
+            </div>
+          ))}
         </div>
-        <div className="app__gallery-images-arrow">
-          <BsArrowLeftShort className="gallery_arrow-icon" onClick={() => { scroll('left') }} />
-          <BsArrowRightShort className="gallery_arrow-icon" onClick={() => { scroll('right') }} />
+        <div className="app__gallery-images_arrows">
+          <BsArrowLeftShort className="gallery__arrow-icon" onClick={() => scroll('left')} />
+          <BsArrowRightShort className="gallery__arrow-icon" onClick={() => scroll('right')} />
         </div>
       </div>
-    </div >
-  )
+    </div>
+  );
 };
 
 export default Gallery;
